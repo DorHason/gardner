0b9c4b9b36c75134c06465221ff382e44de56ac8
Dor Hason <dorhason3@gmail.com>
2020-09-02 16:56:54 +0000

    installed redis finally

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-// check
-console.log("check")
+// express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const redis = require('redis');
+const client = redis.createClient(); // this creates a new client
+
+client.on('connect', function() {
+	console.log('Redis client connected');
+});
+
+client.on('error', function (err) {
+	console.log('Something went wrong ' + err);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Server has started on port ${port}`)
+});
